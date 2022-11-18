2643c97ab8ae552d447ae1c4f0c03fa2f6555eca
Felix Hillingshaeuser <felix@mxcore.de>
2022-11-18 21:41:24 +0100

    add cmd/root.go

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "AoC2022 [--example] [day]",
+	Short: "Advent of Code 2022",
+	Long:  "AoC2022",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("example", "e", false, "Use the example input")
+}
