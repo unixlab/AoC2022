db3c625b4d03a4c6d88afcc94faa14e35c147509
Felix Hillingshaeuser <felix@mxcore.de>
2022-11-18 22:51:05 +0100

    add day02 skeleton

---
+++ cmd/day02.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/unixlab/AoC2022/internal/aoeinput"
+	"github.com/unixlab/AoC2022/internal/day02"
+)
+
+// day02Cmd represents the day02 command
+var day02Cmd = &cobra.Command{
+	Use: "day02",
+	Run: func(cmd *cobra.Command, args []string) {
+		example, err := cmd.Flags().GetBool("example")
+		if err != nil {
+			panic(err)
+		}
+		day02.RunPart1(aoeinput.Read(cmd.Use, example))
+		day02.RunPart2(aoeinput.Read(cmd.Use, example))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(day02Cmd)
+}
