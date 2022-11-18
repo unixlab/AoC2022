56ea9cae35aa6e1b569f0dd41a0b0a8f0786ca6b
Felix Hillingshaeuser <felix@mxcore.de>
2022-11-18 22:50:43 +0100

    add dayXX as template

---
+++ cmd/dayXX.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/unixlab/AoC2022/internal/aoeinput"
+	"github.com/unixlab/AoC2022/internal/dayXX"
+)
+
+// dayXXCmd represents the dayXX command
+var dayXXCmd = &cobra.Command{
+	Use: "dayXX",
+	Run: func(cmd *cobra.Command, args []string) {
+		example, err := cmd.Flags().GetBool("example")
+		if err != nil {
+			panic(err)
+		}
+		dayXX.RunPart1(aoeinput.Read(cmd.Use, example))
+		dayXX.RunPart2(aoeinput.Read(cmd.Use, example))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dayXXCmd)
+}
---
+++ internal/dayXX/main.go
@@ -0,0 +1,15 @@
+package dayXX
+
+import "fmt"
+
+func RunPart1(input []string) {
+	for _, line := range input {
+		fmt.Println(line)
+	}
+}
+
+func RunPart2(input []string) {
+	for _, line := range input {
+		fmt.Println(line)
+	}
+}
