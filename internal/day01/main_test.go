8df8576c1670beff6fd7058f1367f8fe08bb9fed
Felix Hillingshaeuser <felix@mxcore.de>
2022-12-05 08:23:28 +0100

    add tests for day01

---
+++ internal/day01/main_test.go
@@ -0,0 +1,23 @@
+package day01
+
+import (
+	"testing"
+
+	"github.com/unixlab/AoC2022/internal/aoeinput"
+)
+
+func TestRunPart1(t *testing.T) {
+	expectedResult := 24000
+	actualResult := RunPart1(aoeinput.Read("../../", "day01", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart1() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2(t *testing.T) {
+	expectedResult := 45000
+	actualResult := RunPart2(aoeinput.Read("../../", "day01", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
