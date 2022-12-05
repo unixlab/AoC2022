b897ab9e7c92f5faec9d8582e13d248eed3cebaa
Felix Hillingshaeuser <felix@mxcore.de>
2022-12-05 08:41:55 +0100

    add more test cases to template

--- internal/dayXX/main_test.go
+++ internal/dayXX/main_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/unixlab/AoC2022/internal/aoeinput"
 )
 
-func TestRunPart1(t *testing.T) {
+func TestRunPart1Example(t *testing.T) {
 	expectedResult := 0
 	actualResult := RunPart1(aoeinput.Read("../../", "dayXX", true))
 	if actualResult != expectedResult {
@@ -14,9 +14,25 @@ func TestRunPart1(t *testing.T) {
 	}
 }
 
-func TestRunPart2(t *testing.T) {
+func TestRunPart1(t *testing.T) {
+	expectedResult := 0
+	actualResult := RunPart1(aoeinput.Read("../../", "dayXX", false))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart1() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2Example(t *testing.T) {
 	expectedResult := 0
 	actualResult := RunPart2(aoeinput.Read("../../", "dayXX", true))
+	if actualResult != expectedResult {
+		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
+	}
+}
+
+func TestRunPart2(t *testing.T) {
+	expectedResult := 0
+	actualResult := RunPart2(aoeinput.Read("../../", "dayXX", false))
 	if actualResult != expectedResult {
 		t.Fatalf("RunPart2() = expected %v, got %v", expectedResult, actualResult)
 	}
