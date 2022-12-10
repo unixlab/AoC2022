e36cb7c4a81720ccf42244cd77b34a95e7096273
Felix Hillingshaeuser <felix@mxcore.de>
2022-12-10 10:39:17 +0100

    add day10

---
+++ cmd/day10.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/unixlab/AoC2022/internal/aoeinput"
+	"github.com/unixlab/AoC2022/internal/day10"
+)
+
+// day10Cmd represents the day10 command
+var day10Cmd = &cobra.Command{
+	Use: "day10",
+	Run: func(cmd *cobra.Command, args []string) {
+		example, err := cmd.Flags().GetBool("example")
+		if err != nil {
+			panic(err)
+		}
+		input := aoeinput.Read("", cmd.Use, example)
+		part1, part2 := day10.Run(input, false)
+		fmt.Printf("day10 part 1 => %d\n", part1)
+		fmt.Printf("day10 part 2 => \n%s", part2)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(day10Cmd)
+}
---
+++ internal/day10/main.go
@@ -0,0 +1,68 @@
+// Package day10 is our package for the 10th AoC day
+package day10
+
+import (
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+type Instruction struct {
+	Cycles int
+	Value  int
+}
+
+// absDiff calculates the absolut difference as int
+func absDiff(x int, y int) int {
+	return int(math.Abs(float64(x) - float64(y)))
+}
+
+// Run as two return values and can be used for both parts
+func Run(input []string, printColor bool) (int, string) {
+	var instructions []Instruction
+	for _, line := range input {
+		var newInstruction Instruction
+		switch strings.Split(line, " ")[0] {
+		case "addx":
+			newInstruction.Cycles = 2
+			newInstruction.Value, _ = strconv.Atoi(strings.Split(line, " ")[1])
+		case "noop":
+			newInstruction.Cycles = 1
+			newInstruction.Value = 0
+		default:
+			return -1, ""
+		}
+		instructions = append(instructions, newInstruction)
+	}
+	idx := 0
+	regX := 1
+	signalStrength := 0
+	spriteLit := "#"
+	spriteDark := "."
+	if printColor {
+		spriteLit = color.New(color.BgWhite).Sprint(" ")
+		spriteDark = color.New(color.BgBlack).Sprint(" ")
+	}
+	var sprite strings.Builder
+	for cycle := 1; cycle <= 240; cycle++ {
+		if absDiff((cycle-1)%40, regX) < 2 {
+			sprite.WriteString(spriteLit)
+		} else {
+			sprite.WriteString(spriteDark)
+		}
+		if cycle%40 == 0 {
+			sprite.WriteString("\n")
+		}
+		if cycle%40 == 20 {
+			signalStrength += regX * cycle
+		}
+		instructions[idx].Cycles--
+		if instructions[idx].Cycles == 0 {
+			regX += instructions[idx].Value
+			idx++
+		}
+	}
+	return signalStrength, sprite.String()
+}
